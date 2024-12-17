8b3a56685edba18603a29852e3de7f7a6ca2a93b
Dada Teniola <emmatenny2004@gmail.com>
2024-12-17 20:38:06 +0100

    testimonials names and profile pictures now match, and all stars have been changed to 5

--- components/testimonials/data.ts
+++ components/testimonials/data.ts
@@ -13,15 +13,15 @@ import Preview7 from "@/public/images/testimonials/previews/preview (7).png";
 import Preview8 from "@/public/images/testimonials/previews/preview (8).png";
 import Preview9 from "@/public/images/testimonials/previews/preview (9).png";
 // ------ Profile Pictures ------
-import PFP1 from "@/public/images/testimonials/profile pictures/pfp (1).png";
-import PFP2 from "@/public/images/testimonials/profile pictures/pfp (2).png";
-import PFP3 from "@/public/images/testimonials/profile pictures/pfp (3).png";
-import PFP4 from "@/public/images/testimonials/profile pictures/pfp (4).png";
-import PFP5 from "@/public/images/testimonials/profile pictures/pfp (5).png";
-import PFP6 from "@/public/images/testimonials/profile pictures/pfp (6).png";
-import PFP7 from "@/public/images/testimonials/profile pictures/pfp (7).png";
-import PFP8 from "@/public/images/testimonials/profile pictures/pfp (8).png";
-import PFP9 from "@/public/images/testimonials/profile pictures/pfp (9).png";
+import PFP1 from "@/public/images/testimonials/profile pictures/design queen.png";
+import PFP2 from "@/public/images/testimonials/profile pictures/anjola adekunle.png";
+import PFP3 from "@/public/images/testimonials/profile pictures/olatide.png";
+import PFP4 from "@/public/images/testimonials/profile pictures/ayo raphael.png";
+import PFP5 from "@/public/images/testimonials/profile pictures/david fayemi.png";
+import PFP6 from "@/public/images/testimonials/profile pictures/ozenua oluwatobi.png";
+import PFP7 from "@/public/images/testimonials/profile pictures/muna.png";
+import PFP8 from "@/public/images/testimonials/profile pictures/sauce.png";
+import PFP9 from "@/public/images/testimonials/profile pictures/kenenna anyaegbunam.png";
 
 export const testimonials_data: TestimonialsData[] = [
   {
@@ -29,55 +29,55 @@ export const testimonials_data: TestimonialsData[] = [
     pfp: PFP1,
     preview: Preview1,
     name: "Design Queen",
-    desc: "I love DesignWhiz for its ease of use & the fact that I can get all my design needs in one place. It’s built with love, for designers, by designers. Sign up!",
+    desc: "I love DesignWhiz for its ease of use & the fact that I can get all my design needs in one place. It's built with love, for designers, by designers. Sign up!",
   },
   {
-    stars: 4,
+    stars: 5,
     pfp: PFP2,
     preview: Preview2,
     name: "Anjola Adekunle",
     desc: "The ability to resize and adjust the number of frames I see at once is so cool. So much flexibility.",
   },
   {
-    stars: 3,
+    stars: 5,
     pfp: PFP3,
     preview: Preview3,
     name: "Olatide",
-    desc: "I love the resources page! It’s such a fantastic feature that not only provides links to valuable resources but also gives credits to the creators.",
+    desc: "I love the resources page! It's such a fantastic feature that not only provides links to valuable resources but also gives credits to the creators.",
   },
   {
-    stars: 2,
+    stars: 5,
     pfp: PFP4,
     preview: Preview4,
     name: "Ayo Raphael",
     desc: "Importing designs from various platforms is such a cool and much necessary feature/addition.",
   },
   {
-    stars: 1,
+    stars: 5,
     pfp: PFP5,
     preview: Preview5,
-    name: "Ozenua Oluwatobi",
+    name: "David Fayemi",
     desc: "Being able to filter by creatives I follow is such a thoughtful feature! Plus, seeing similar frames and hiding the comments bar enhances the experience.",
   },
   {
-    stars: 2,
+    stars: 5,
     pfp: PFP6,
     preview: Preview6,
-    name: "Olalekan 'Sauce' Owoeye",
+    name: "Ozenua Oluwatobi",
     desc: "DesignWhiz design is so well done! Functional and very usable. New updates are so good and are now a 10X upgrade!",
   },
   {
-    stars: 3,
+    stars: 5,
     pfp: PFP7,
     preview: Preview7,
-    name: "David Fayemi",
-    desc: "I like how easy it is to find the creator of any resource! It’s like having my homework done for me. Plus, the ability to share to various platforms is chef kiss!",
+    name: "Muna",
+    desc: "I like how easy it is to find the creator of any resource! It's like having my homework done for me. Plus, the ability to share to various platforms is chef kiss!",
   },
   {
-    stars: 4,
+    stars: 5,
     pfp: PFP8,
     preview: Preview8,
-    name: "Muna",
+    name: "Olalekan ‘sauce’ Owoeye",
     desc: "This is a useful platform for the modern-day creative. The sleek interfaces make the experience very much entertaining and interesting. Get on it fast!",
   },
   {
